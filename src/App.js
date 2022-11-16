b3b75232b3f6cb8e6d9fc85e599d1ba918f71d54
Paulos Dessie Nedaw <pauldessie@gmail.com>
2022-11-16 23:05:56 +0300

    slight modification

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ function App() {
   return (
     <div className="h-screen bg-black/80 flex items-center justify-center">
       <div className="max-w-2xl mx-auto bg-black/40 text-white flex flex-col md:flex-row rounded-lg items-center">
-        <div className="md:p-3">
-          <img src={image6} alt="plant" className="w-full md:w-64 my-3 md:my-0 h-64 rounded-l-lg transform hover:scale-110 hover:rounded-lg duration-300"/>
+        <div className="w-full md:w-64 p-6 md:p-3">
+          <img src={image6} alt="plant" className="w-full  my-3 md:my-0 h-64 rounded-l-lg transform hover:scale-110 hover:rounded-lg duration-300"/>
         </div>
 
         <div className="mx-auto flex flex-col gap-4 px-8 font-serif">
@@ -14,7 +14,7 @@ function App() {
           <p className="text-white/70">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, reprehenderit!</p>
           <div className="flex flex-col md:flex-row gap-4 my-5">
             <input type="text" placeholder="Enter your email address" className="border border-white/20 bg-black/20 w-full text-white py-2 px-5 focus:outline-none"/>
-            <button type="submit" className="bg-green-600 rounded-lg px-5 h-12">Subscribe</button>
+            <button type="submit" className="bg-green-600 rounded-lg px-5 h-12 transform hover:scale-105 hover:bg-lime-600 hover:text-zinc-800 duration-200">Subscribe</button>
           </div>
         </div>
 
