a557279411f762ae9346d7922b7fe744e7e4f004
Paulos Dessie Nedaw <pauldessie@gmail.com>
2022-11-17 23:07:22 +0300

    price tags complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,176 @@
-import image6 from "./img/img6.jpg";
-
 function App() {
   
   return (
-    <div className="h-screen bg-zinc-700 flex items-center justify-center gap-8">
-      <div className="max-w-sm md:max-w-2xl bg-zinc-800 text-white flex flex-col md:flex-row rounded-lg items-center">
-        <div className="w-full md:w-64 p-3">
-          <img src={image6} alt="plant" className="w-full h-64 rounded-l-lg transform hover:scale-105 hover:rounded-lg duration-300"/>
-        </div>
-
-        <div className="mx-auto flex flex-col gap-4 px-8 font-serif">
-          <h1 className="text-2xl">Get diet and fitness tips in your inbox</h1>
-          <p className="text-white/70">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, reprehenderit!</p>
-          <div className="flex flex-col md:flex-row gap-4 my-5">
-            <input type="text" placeholder="Enter your email address" className="border border-white/20 bg-zinc-800 w-full text-white py-2 px-5 focus:outline-none"/>
-            <button type="submit" className="bg-green-600 rounded-lg px-5 h-12 transform hover:scale-105 hover:bg-lime-600 hover:text-zinc-800 duration-200">Subscribe</button>
+    <div className="flex justify-center items-center bg-slate-800 text-white">
+      <div className="my-10 flex flex-col md:flex-row md:my-0 md:h-screen justify-center md:items-center gap-4">
+        <div className="h-100 md:h-128 w-72 border-8 border-slate-700 rounded-lg flex flex-col items-center">
+          <h3 className="my-5 text-xl">Basic</h3>
+          <h1 className="text-4xl font-serif">100GB</h1>
+          <p className="text-md">$1.99/month</p>
+          <button className="my-6 outline outline-1 outline-purple-600 hover:bg-purple-600 hover:outline-none px-8 py-2 rounded-md">Purchase</button>
+          <div className="w-full mt-10 pt-8 border border-b-0 border-slate-700 flex flex-col gap-1">
+            <div className="flex gap-2 justify-center">
+              <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="w-5 h-5"
+                  viewBox="0 0 24 24"
+                  stroke-width="2"
+                  stroke="currentColor"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <path d="M5 12l5 5l10 -10" />
+                </svg>
+              <p className="text-md">100 GB storage</p>
+            </div>
+            <div className="flex gap-2 justify-center">
+              <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="w-5 h-5"
+                  viewBox="0 0 24 24"
+                  stroke-width="2"
+                  stroke="currentColor"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <path d="M5 12l5 5l10 -10" />
+                </svg>
+              <p className="text-md">Option to add members</p>
+            </div>
+            <div className="flex gap-2 justify-center">
+              <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="w-5 h-5"
+                  viewBox="0 0 24 24"
+                  stroke-width="2"
+                  stroke="currentColor"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <path d="M5 12l5 5l10 -10" />
+                </svg>
+              <p className="text-md">Extra member benefits</p>
+            </div>
           </div>
         </div>
-
+        <div className="h-100 md:h-128 w-72 border-8 border-purple-600 rounded-lg flex flex-col items-center">
+          <h3 className="my-5 text-xl">Standard</h3>
+          <h1 className="text-4xl font-serif">200GB</h1>
+          <p className="text-md">$3.99/month</p>
+          <button className="my-6 hover:outline hover:outline-1 hover:outline-none hover:bg-slate-700 bg-purple-600 px-8 py-2 rounded-md">Purchase</button>
+          <div className="w-full mt-10 pt-8 border border-b-0 border-slate-700 flex flex-col gap-1">
+            <div className="flex gap-2 justify-center">
+              <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="w-5 h-5"
+                  viewBox="0 0 24 24"
+                  stroke-width="2"
+                  stroke="currentColor"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <path d="M5 12l5 5l10 -10" />
+                </svg>
+              <p className="text-md">200 GB storage</p>
+            </div>
+            <div className="flex gap-2 justify-center">
+              <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="w-5 h-5"
+                  viewBox="0 0 24 24"
+                  stroke-width="2"
+                  stroke="currentColor"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <path d="M5 12l5 5l10 -10" />
+                </svg>
+              <p className="text-md">Option to add members</p>
+            </div>
+            <div className="flex gap-2 justify-center">
+              <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="w-5 h-5"
+                  viewBox="0 0 24 24"
+                  stroke-width="2"
+                  stroke="currentColor"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <path d="M5 12l5 5l10 -10" />
+                </svg>
+              <p className="text-md">Extra member benefits</p>
+            </div>
+          </div>
+        </div>
+        <div className="h-100 md:h-128 w-72 border-8 border-slate-700 rounded-lg flex flex-col items-center">
+          <h3 className="my-5 text-xl">Premium</h3>
+          <h1 className="text-4xl font-serif">2TB</h1>
+          <p className="text-md">$8.99/month</p>
+          <button className="my-6 outline outline-1 outline-purple-600 hover:bg-purple-600 hover:outline-none px-8 py-2 rounded-md">Purchase</button>
+          <div className="w-full mt-10 pt-8 border border-b-0 border-slate-700 flex flex-col gap-1">
+            <div className="flex gap-2 justify-center">
+              <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="w-5 h-5"
+                  viewBox="0 0 24 24"
+                  stroke-width="2"
+                  stroke="currentColor"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <path d="M5 12l5 5l10 -10" />
+                </svg>
+              <p className="text-md">2 TB storage</p>
+            </div>
+            <div className="flex gap-2 justify-center">
+              <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="w-5 h-5"
+                  viewBox="0 0 24 24"
+                  stroke-width="2"
+                  stroke="currentColor"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <path d="M5 12l5 5l10 -10" />
+                </svg>
+              <p className="text-md">Option to add members</p>
+            </div>
+            <div className="flex gap-2 justify-center">
+              <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="w-5 h-5"
+                  viewBox="0 0 24 24"
+                  stroke-width="2"
+                  stroke="currentColor"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <path d="M5 12l5 5l10 -10" />
+                </svg>
+              <p className="text-md">Extra member benefits</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
